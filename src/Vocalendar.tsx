2a4f8e63677a05bd82ccba78ce3264379316b367
Mizuki Ryou <ryou@maple.dti2.ne.jp>
2022-07-09 07:43:54 +0000

    フィルタ機能の実装、日時表示をxx:xx形式に変更、イベントクリックでのイベント仮実装

--- src/Vocalendar.tsx
+++ src/Vocalendar.tsx
@@ -1,22 +1,33 @@
-import React, { useRef, VFC } from 'react';
-import FullCalendar, { CustomButtonInput, EventInput, } from '@fullcalendar/react';
+import React, { useRef, useState, VFC } from 'react';
+import FullCalendar, { CustomButtonInput, EventInput, formatDate } from '@fullcalendar/react';
 import dayGridPlugin from "@fullcalendar/daygrid"
 import timeGridPlugin from "@fullcalendar/timegrid"
+import integrationPlugIn from "@fullcalendar/interaction"
+import { info } from 'console';
 
 const Vocalendar: VFC = () => {
 
+  const [isHideLongEvent, setHideLongEvent] = useState(false)
+
   // FullCalendarのインスタンスを取得
-  const calendarRef = useRef<FullCalendar>(null!)
+  // const calendarRef = useRef<FullCalendar>(null!)
 
   const getEvents = (events: EventInput[]) => {
+
     // EventDataの書き換えはeventDataTransformでもできる
-    events.forEach((event) => {
+    events.filter((event)=>{
+      // TODO 1日より長い期間のあるイベントを除外したい
+      // const nextDay = event.setDate(event.getDate() + 1)
+      return (!isHideLongEvent) || !event.allday
+    })
+    .forEach((event) => {
       event.allDay = event.allday;
       event.start = event.start_datetime;
       event.end = event.end_datetime;
-      event.title = event.summary;
+      event.title = event.tags[0]?.name + ':' + event.summary;
       if ((!event.allDay) && event.start) {
-        //formatDate(event.start,"yyyy/mm/dd")
+        // formatDate(event.start,{          
+        // })
       }
     })
 
@@ -24,13 +35,9 @@ const Vocalendar: VFC = () => {
   }
 
   const customButton: CustomButtonInput = {
-    text: 'TEST',
+    text: 'HideDayEvent',
     click: (event) => {
-      alert("a")
-      //うごかない
-      customButton.text = 'CLICK'
-      // 現在未使用
-      calendarRef.current.getApi()
+      setHideLongEvent(!isHideLongEvent)
     }
 
   }
@@ -38,7 +45,7 @@ const Vocalendar: VFC = () => {
   return (
     <div className="App">
       <FullCalendar
-        plugins={[dayGridPlugin, timeGridPlugin]}
+        plugins={[dayGridPlugin, timeGridPlugin, integrationPlugIn]}
         initialView="dayGridMonth"
         customButtons={{ customButton }}
         headerToolbar={{
@@ -54,7 +61,7 @@ const Vocalendar: VFC = () => {
         //events = {(info, succesCallback, failureCallback)=>{}}
         eventSources={[
           {
-            url: 'http://localhost:3000/core/events.json',
+            url: 'core/events.json',
             method: 'GET',
             format: 'json',
             startParam: 'startTime', // URLパラメータに入れる取得開始時間
@@ -64,10 +71,21 @@ const Vocalendar: VFC = () => {
             }
           }
         ]}
+        height={480}
         eventSourceSuccess={getEvents}
         eventSourceFailure={(error: any) => {
           console.log(error);
         }}
+        eventTimeFormat={{
+          hour: 'numeric',
+          minute: '2-digit',
+          meridiem: false
+        }}
+        // うごかない？
+        selectable={true}
+        select={(info)=>{alert(info)}}
+
+        eventClick={(e)=>{alert(e)}}
 
       />
     </div>
