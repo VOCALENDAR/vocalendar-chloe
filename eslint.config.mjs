239a77d31473079aa8e9aebd7422a013404eddd1
Ryou Mizuki <ryou@maple.dti2.ne.jp>
2024-07-14 23:16:01 +0900

    Eslintをちゃんと設定。まだVSCodeでリアルタイムで動かない

---
+++ eslint.config.mjs
@@ -0,0 +1,30 @@
+import globals from "globals";
+import eslint from "@eslint/js";
+import ts from "typescript-eslint";
+import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
+import { fixupConfigRules } from "@eslint/compat";
+
+export default ts.config({
+  files: ["src/**/*.{ts,tsx}"],
+  extends: [
+    eslint.configs.recommended,
+    ...ts.configs.recommended,
+    ...fixupConfigRules(pluginReactConfig),
+  ],
+  languageOptions: { 
+    parserOptions: { ecmaFeatures: { jsx: true } },
+    globals: {
+      ...globals.browser,
+      ...globals.es2022,
+      }
+   },
+   settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  rules: {
+    "no-console": "warn",
+  },
+  ignores: ["build/*","node_modules/*", "vite.config.js"]
+})
