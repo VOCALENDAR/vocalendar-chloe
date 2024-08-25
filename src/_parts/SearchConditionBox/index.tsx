e1c4834e2985617a710802e119e5677636a4a42e
Ryou Mizuki <ryou@maple.dti2.ne.jp>
2024-08-25 18:19:21 +0900

    検索ボックスを別コンポーネントに切り出す

---
+++ src/_parts/SearchConditionBox/index.tsx
@@ -0,0 +1,11 @@
+type Props = Record<string, never>
+
+/**
+ * 検索条件入力欄を構築する
+ * @returns
+ */
+const SearchConditionBox: React.FC<Props> = _props => {
+  return <></>
+}
+
+export default SearchConditionBox
