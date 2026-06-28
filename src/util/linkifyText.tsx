e8c6a40fc6eb96c8fdcb69b7608660807222118e
Mizuki Ryou <ryou@maple.dti2.ne.jp>
2026-06-28 15:11:35 +0900

    OgImageに渡すURLが変わってしまっていた？ため修正

--- src/util/linkifyText.tsx
+++ src/util/linkifyText.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
-const URL_REGEX = /(https?:\/\/[^\s]+)/g
+const URL_REGEX = /(https?:\/\/[^\s]+)/
 
 const linkifyText = (text: string): React.ReactNode[] =>
   text.split(URL_REGEX).map((part, i) =>
-    URL_REGEX.test(part) ? (
+    part.startsWith('http://') || part.startsWith('https://') ? (
       <a key={i} href={part} target="_blank" rel="noopener noreferrer">
         {part}
       </a>
